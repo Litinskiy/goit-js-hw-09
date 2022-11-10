2282bfada28868ff91899130b5f2a624036afe02
Litinskiy <111707701+Litinskiy@users.noreply.github.com>
2022-11-10 19:18:14 +0000

    Deploying to gh-pages from @ Litinskiy/goit-js-hw-09@1f52f0ba5bb753cfc2736443c60e7ea2c447c0aa 🚀

---
+++ 01-color-switcher.735eff1f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");let o=null;e.addEventListener("click",(function(){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),n.addEventListener("click",(function(){clearInterval(o),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.735eff1f.js.map
